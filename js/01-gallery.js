ec7463e39830f02e0933133d0a2da060d0e4cc35
vvv-k <vkorotlov@gmail.com>
2023-01-09 04:28:57 +0300

    forth commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,31 +3,75 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryEl = document.querySelector(".gallery");
 const images = galleryItems
-    .map((image) => {
+    .map((image, index) => {
         const html = `<div class = "gallery__item">
     <a class = "gallery__link" href="${image.original}">
-    <img loading="lazy" class="gallery__image" 
+    <img loading="lazy" data-set="${image.original}" class="gallery__image" 
     src="${image.preview}"
-    alt="${image.description}"/></a></div>`;
+    alt="${image.description}"
+    data-index = "${index}"
+    /></a></div>`;
         return html;
     })
     .join("");
 galleryEl.insertAdjacentHTML('beforeend', images);
 
+let currentIndex = 0;
+
+
+const instance = basicLightbox.create(
+    `
+    <img src="" alt="" width = "1280" />
+    `, {
+    onShow: (instance) => {
+        document.onkeydown = function (e) {
+            if (e.key === "Escape" && instance.visible()) {
+                instance.close();
+                return;
+            }
+            if (e.key === "ArrowLeft" && instance.visible()) {
+                /*  if (currentIndex === 0) currentIndex = galleryEl.length;
+                  const elem = instance.element().querySelector("img");
+                  const nodesImg = galleryEl.querySelectorAll('img');
+                  const newImg = nodesImg[(Math.abs(currentIndex - 1)) % 9];
+                  elem.src = newImg.dataset.set;
+                  currentIndex = currentIndex - 1;*/
+                currentIndex = currentIndex - 1;
+                if (currentIndex === -1) currentIndex = galleryItems.length - 1;
+                const elem = instance.element().querySelector("img");
+                elem.src = galleryItems[currentIndex].original;
+
+            }
+            if (e.key === "ArrowRight" && instance.visible()) {
+                currentIndex = currentIndex + 1;
+                if (currentIndex === galleryItems.length) currentIndex = 0;
+                const elem = instance.element().querySelector("img");
+                elem.src = galleryItems[currentIndex].original;
+
+                /* if (currentIndex === galleryEl.length) currentIndex = 0;
+                 const elem = instance.element().querySelector("img");
+                 const nodesImg = galleryEl.querySelectorAll('img');
+                 const newImg = nodesImg[(Math.abs(currentIndex + 1)) % 9];
+                 elem.src = newImg.dataset.set;
+                 currentIndex = currentIndex + 1;*/
+            }
+        }
+    },
+    onClose: (instance) => { document.onkeydown = '' },
+}
+);
+
 
 galleryEl.addEventListener("click", (event) => {
     if (event.target.tagName !== 'IMG') return;
+    //  console.log(event.target.outerHTML);
     event.preventDefault();
-    let instance = basicLightbox.create(
-        `<img loading="lazy" src="${event.target.parentNode.href}" alt = "${event.target.alt}" />`);
+    console.log(event.target.dataset.set);
+    const elem = instance.element().querySelector("img");
+    elem.src = event.target.dataset.set;
+    elem.alt = event.target.alt;
+    currentIndex = Number(event.target.dataset.index);
     instance.show();
-    document.onkeydown = '';
-    document.onkeydown = function (e) {
-        if (e.key === "Escape" && instance.visible()) {
-            instance.close();
-            document.onkeydown = '';
-        }
-    };
 })
 
 
